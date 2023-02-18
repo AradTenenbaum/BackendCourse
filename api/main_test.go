2045b471dafdeecc227836f79c4b6145486ab11b
Arad Tenenbaum <arad.tenen@gmail.com>
2023-02-18 22:05:37 +0200

    'token-login-middleware'

--- api/main_test.go
+++ api/main_test.go
@@ -3,19 +3,22 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
 	db "github.com/AradTenenbaum/BackendCourse/db/sqlc"
+	"github.com/AradTenenbaum/BackendCourse/util"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
 )
 
 func newTestServer(t *testing.T, store db.Store) *Server {
-	// config := util.Config{
-	// 	// TokenSymmetricKey:   util.RandomString(32),
-	// 	// AccessTokenDuration: time.Minute,
-	// }
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
 
-	server := NewServer(store)
-	// require.NoError(t, err)
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
 
 	return server
 }
