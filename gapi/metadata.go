1505167f14f3688e5ae4be5cf834ce27cf0d1b2c
Arad Tenenbaum <arad.tenen@gmail.com>
2023-02-28 15:54:46 +0200

    'grpc-gateway'

---
+++ gapi/metadata.go
@@ -0,0 +1,45 @@
+package gapi
+
+import (
+	"context"
+
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
+)
+
+const (
+	grpcGatewayUserAgentHeader = "grpcgateway-user-agent"
+	userAgentHeader            = "user-agent"
+	xForwardedForHeader        = "x-forwarded-for"
+)
+
+type Metadata struct {
+	UserAgent string
+	ClientIP  string
+}
+
+func (server *Server) extractMetadata(ctx context.Context) *Metadata {
+	mtdt := &Metadata{}
+
+	// Get metadata - grpc and http connections(except client ip)
+	if md, ok := metadata.FromIncomingContext(ctx); ok {
+		if userAgents := md.Get(grpcGatewayUserAgentHeader); len(userAgents) > 0 {
+			mtdt.UserAgent = userAgents[0]
+		}
+
+		if userAgents := md.Get(userAgentHeader); len(userAgents) > 0 {
+			mtdt.UserAgent = userAgents[0]
+		}
+
+		if clientIps := md.Get(xForwardedForHeader); len(clientIps) > 0 {
+			mtdt.ClientIP = clientIps[0]
+		}
+	}
+
+	// Get ip from metadata
+	if p, ok := peer.FromContext(ctx); ok {
+		mtdt.ClientIP = p.Addr.String()
+	}
+
+	return mtdt
+}
